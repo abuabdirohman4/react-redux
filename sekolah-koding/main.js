b94687ae6225cb97d2448db8d7e1dfe1fda6d009
abuabdirohman4 <abuabdirohman4@gmail.com>
2022-09-29 16:18:32 +0700

    sk : Membuat fungsi createstore sendiri

--- sekolah-koding/main.js
+++ sekolah-koding/main.js
@@ -1,7 +1,34 @@
 // Aplikadi Redux
 
 // Store(reducer)
-let store = Redux.createStore(counter);
+// let store = Redux.createStore(counter);
+
+const createStore = (reducer) => {
+  let state;
+  let listeners = [];
+
+  const getState = () => state;
+
+  const dispatch = (action) => {
+    state = reducer(state, action);
+    listeners.forEach((listener) => listener());
+  };
+
+  const subscribe = (listener) => {
+    listeners.push(listener);
+
+    // unsubscribe
+    return () => {
+        listeners = listeners.filter(l => listener)
+    }
+  };
+
+  dispatch({})
+
+  return {getState, dispatch, subscribe}
+};
+
+let store = createStore(counter);
 
 // Reducer(state, action)
 function counter(state = 0, action) {
@@ -18,7 +45,7 @@ function counter(state = 0, action) {
 }
 
 // Subscribe
-store.subscribe(() => console.log('state saat ini ...', store.getState()));
+store.subscribe(() => console.log("state saat ini ...", store.getState()));
 
 // Action
 store.dispatch({ type: "INCREMENT" });
