3d466c650c01a6eb3c14088560034eda73d4f561
abuabdirohman4 <abuabdirohman4@gmail.com>
2022-10-04 18:20:04 +0700

    Fikri : Setup Edit Product

--- fikri/src/App.js
+++ fikri/src/App.js
@@ -1,16 +1,17 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import AddProduct from "./components/AddProduct";
 import { ShowProduct } from "./components/ShowProduct";
-
+import AddProduct from "./components/AddProduct";
+import EditProduct from "./components/EditProduct";
 
 function App() {
   return (
     <BrowserRouter>
       <div className="container">
         <Routes>
-          <Route path="/" element={<ShowProduct/>}/>
-          <Route path="add" element={<AddProduct/>}/>
+          <Route path="/" element={<ShowProduct />} />
+          <Route path="add" element={<AddProduct />} />
+          <Route path="edit/:id" element={<EditProduct />} />
         </Routes>
       </div>
     </BrowserRouter>
