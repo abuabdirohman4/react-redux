a5367596d1f8554133acf29e12752641a15cda33
abuabdirohman4 <abuabdirohman4@gmail.com>
2022-10-04 18:18:09 +0700

    Fikri : Setup routing

--- fikri/src/App.js
+++ fikri/src/App.js
@@ -1,19 +1,19 @@
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import AddProduct from "./components/AddProduct";
 import { ShowProduct } from "./components/ShowProduct";
 
+
 function App() {
   return (
-    <div className="container">
-      <div className="columns">
-        <div className="column">
-          <AddProduct />
-        </div>
-        <div className="column">
-          <ShowProduct />
-        </div>
+    <BrowserRouter>
+      <div className="container">
+        <Routes>
+          <Route path="/" element={<ShowProduct/>}/>
+          <Route path="add" element={<AddProduct/>}/>
+        </Routes>
       </div>
-    </div>
+    </BrowserRouter>
   );
 }
 
