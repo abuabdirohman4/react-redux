ce71126e2c9dd682635165c2f708815e95e4b464
abuabdirohman4 <abuabdirohman4@gmail.com>
2022-10-04 17:47:37 +0700

    Fikri : Implement Redux on Add Product

--- fikri/src/components/AddProduct.jsx
+++ fikri/src/components/AddProduct.jsx
@@ -1,27 +1,54 @@
-import React from 'react'
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { update } from "../features/productSlice";
 
 const AddProduct = () => {
+  const [title, setTitle] = useState("");
+  const [price, setPrice] = useState("");
+  const dispatch = useDispatch()
+
+  const updateProduct = (e) => {
+    e.preventDefault();
+    dispatch(update({title, price}))
+  };
+
   return (
     <div>
-      <form action="" className="box mt-5">
+      <form onSubmit={updateProduct} className="box mt-5">
         <div className="field">
-          <label htmlFor="" className="label">Title</label>
-            <div className="control">
-              <input type="text" className="input" placeholder='Title' />
-            </div>
+          <label htmlFor="" className="label">
+            Title
+          </label>
+          <div className="control">
+            <input
+              type="text"
+              className="input"
+              placeholder="Title"
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+            />
+          </div>
         </div>
         <div className="field">
-          <label htmlFor="" className="label">Price</label>
-            <div className="control">
-              <input type="text" className="input" placeholder='Price' />
-            </div>
+          <label htmlFor="" className="label">
+            Price
+          </label>
+          <div className="control">
+            <input
+              type="text"
+              className="input"
+              placeholder="Price"
+              value={price}
+              onChange={(e) => setPrice(e.target.value)}
+            />
+          </div>
         </div>
         <div className="field">
           <button className="button is-success">Submit</button>
         </div>
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default AddProduct
+export default AddProduct;
